286289d524274962fef2026547a88001918939ea
vivowa <victor.wachter.von@gmail.com>
2018-09-24 13:12:10 +0200

    correct call of functions and handling of return

--- test/Test_1_TechnicalRequirements.js
+++ test/Test_1_TechnicalRequirements.js
@@ -2,10 +2,9 @@ const EquityTokenFactory = artifacts.require("./EquityTokenFactory.sol");
 
 contract('TestTechnicalRequirements.js', async (accounts) => {
     
-    // --- Technical Test ---
-    // it("should deploy the correct hierachi")
-    // it("should avoid under/overflow") 
-    
+    // --- Technical Test ---   
+   describe("technical prerequirements", async () => {
+
     it("should call a function that depends on a linked library", async () => {
     let instance = await EquityTokenFactory.deployed();
     let outTokenBalance = await (instance.getBalance.call(accounts[0]));
@@ -14,33 +13,47 @@ contract('TestTechnicalRequirements.js', async (accounts) => {
     let equityTokenEthBalance = outTokenBalanceEth.toNumber();
       assert.equal(equityTokenEthBalance, 2 * equityTokenBalance, "Library function returned unexpected function, linkage may be broken");
     });
+
+    // it("should deploy the correct hierachi")
+    // it("should avoid under/overflow") 
+  });
   
   // --- EquityTokenFactory Test ---
-  
-    it("should issue equity token", async () => {
-      let instance = await EquityTokenFactory.deployed();
-    
+    describe("correct token issuance", async () => {
       const _name = "TestCompany";
       const _ticker = "TCO";
       const _amount = 10000;
       const _nominalvalue = 7;
+      const _indexEquityTokenInArray = 0;
+
+    it("should have a name", async () => {
+      let instance = await EquityTokenFactory.deployed(); 
 
       await instance.createEquityToken(_name, _ticker, _amount, _nominalvalue, {from: accounts[0]});
-      
-      let balance = await instance.getBalance.call(accounts[0]);
-      assert.equal(balance.valueOf(), _amount, "specified amount wasn't in the first account");
+      let information = await instance.getInfosEquityToken(_indexEquityTokenInArray, {from: accounts[0]});
+
+      assert.equal(information[1], _name,"company name missing");
+    });
+   
+    it("should have a ticker", async () => {
+      let instance = await EquityTokenFactory.deployed(); 
+
+      await instance.createEquityToken(_name, _ticker, _amount, _nominalvalue, {from: accounts[0]});
+      let information = await instance.getInfosEquityToken(_indexEquityTokenInArray, {from: accounts[0]});
+
+      assert.equal(information[2], _ticker,"ticker missing");
     });
 
-    // it("should safe information in array")
-
-    it("should put equity token in the first account", async () => {
+    it("should put issuing amount in the first account", async () => {
       let instance = await EquityTokenFactory.deployed();
       let balance = await instance.getBalance.call(accounts[0]);
-      assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+      assert.equal(balance.valueOf(), _amount, "specific amount wasn't in the first account");
     });
-     
+    // it("should safe information in array")
+  });
 
   // --- EquityToken Test ---
+  describe("corrent token transactions", async ()=> {
     it("should send token correctly", async () => {
       let instance = await EquityTokenFactory.deployed();
     
@@ -66,5 +79,6 @@ contract('TestTechnicalRequirements.js', async (accounts) => {
         assert.equal(account_one_ending_balance, account_one_starting_balance - _amount, "Amount wasn't correctly taken from the sender");
         assert.equal(account_two_ending_balance, account_two_starting_balance + _amount, "Amount wasn't correctly sent to the receiver");
         
-    });      
+    });
+  });     
 })
